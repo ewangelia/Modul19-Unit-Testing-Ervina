864c07345f82277edbe32b7d61aa783df36cee8a
angel <angeeliaw@gmail.com>
2022-11-28 21:28:11 +0700

    +

---
+++ reversestring.test.js
@@ -0,0 +1,24 @@
+const reverseString = require('./reversestring');
+
+test('reverseString function exists', () => {
+  expect(reverseString).toBeDefined();
+});
+
+test('String reverses', () => {
+  expect(reverseString('hello')).toEqual('olleh');
+});
+
+test('String reverses', () => {
+  // complete with another equal example
+  expect(reverseString('email')).toEqual('liame');
+  
+});
+
+test('String reverses with uppercase', () => {
+  expect(reverseString('Hello')).toEqual('olleh');
+});
+
+test('String reverses with uppercase', () => {
+  // complete with another uppercase equal example
+  expect(reverseString('Email')).toEqual('liame');
+});
